a60e8305e6aa7ca9b40e2276daf48bed3ace6e5d
Yulianna05k <162246556+Yulianna05k@users.noreply.github.com>
2024-05-18 00:06:28 +0000

    Ejercicio 1

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
-
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
